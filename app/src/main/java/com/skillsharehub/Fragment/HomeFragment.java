c40c9d0bed4e9c7ad80345a6228e78ea2ecb542f
Neha Vaghela <nehavaghela65@gmail.com>
2026-05-09 19:00:35 +0530

    Add code of Bottom navigation and Home fragment

---
+++ app/src/main/java/com/skillsharehub/Fragment/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.skillsharehub.Fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
+import com.skillsharehub.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // 1. Inflate the layout and store it in a View variable
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        // 2. Use the 'view' variable to find your IDs
+        CircularProgressIndicator circularStreak =
+                view.findViewById(R.id.circularStreak);
+
+        // 3. Set your data
+        int currentStreak = 14;
+        int goal = 30;
+
+        if (circularStreak != null) {
+            circularStreak.setMax(goal);
+            circularStreak.setProgress(currentStreak);
+        }
+
+
+
+        // 4. Return the view
+        return view;
+    }
+}
---
+++ app/src/main/java/com/skillsharehub/MainActivity.java
@@ -0,0 +1,58 @@
+package com.skillsharehub;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.skillsharehub.Fragment.CommunityFragment;
+import com.skillsharehub.Fragment.HomeFragment;
+import com.skillsharehub.Fragment.ProfileFragment;
+import com.skillsharehub.Fragment.ProjectFragment;
+import com.skillsharehub.Fragment.ResourceFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bnview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bnview= findViewById(R.id.bnview);
+        bnview.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.home) {
+                    loadFrag(new HomeFragment(), false);
+                } else if (id == R.id.resources) {
+                    loadFrag(new ResourceFragment(), true);
+                } else if (id == R.id.project) {
+                    loadFrag(new ProjectFragment(), true);
+                } else if (id == R.id.community) {
+                    loadFrag(new CommunityFragment(), true);
+                } else if (id == R.id.profile) {
+                    loadFrag(new ProfileFragment(),true);
+                }
+                return true;
+            }
+
+        });
+
+    }
+    public void loadFrag (Fragment fragment,boolean flag) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.container, fragment);
+        ft.commit();
+    }
+}
